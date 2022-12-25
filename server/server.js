fbdf89064c88dba2d9594935e826c7abc6de283b
rohit-rai-0101rm <rohitrai.dev@gmail.com>
2022-12-25 12:21:29 +0530

    pushing ...

---
+++ server/server.js
@@ -0,0 +1,39 @@
+import express, { response } from "express";
+import * as dotenv from "dotenv";
+import cors from "cors";
+import { Configuration, OpenAIApi } from "openai";
+dotenv.config();
+console.log(process.env.OPEN_API_KEY)
+const configuration = new Configuration({
+  apiKey: process.env.OPEN_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.get("/", async (req, res) => {
+  res.status(200).send({
+    message: "Hello from Likh-Chit",
+  });
+});
+app.post("/", async (req, res) => {
+  try {
+    const prompt = req.body.prompt;
+    const response = await openai.createCompletion({
+      model: "text-davinci-003",
+      prompt: `${prompt}`,
+      temperature: 0, //risk
+      max_tokens: 3000, //max number of tokens to genrate in a completion--long responses
+      top_p: 1,
+      frequency_penalty: 0, //not repeat similar senences often
+      presence_penalty: 0,
+    });
+    res.status(200).send({
+      bot: response.data.choices[0].text,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({ error });
+  }
+});
+app.listen(5000,()=>console.log(`Server is running on port http://localhost:5000`))
